3749878b0265dbb66c060d1b7784a6d986fbc659
Michael Rosero <michael.rosero@akold.com>
2025-01-09 16:53:13 -0500

    ajustes videos turnero

--- resources/views/digiturno/portal.blade.php
+++ resources/views/digiturno/portal.blade.php
@@ -831,15 +831,15 @@
     }
 
     async function printTurno(detalle){
-        /*console.log(detalle);
+        console.log(detalle);
         let args = [];
-        args["endpoint"] = `http://localhost:3002/printer-ticket/v1/turnero?turno=${detalle.turno}&sucursal=${detalle.nombreSucursalTurnero.toUpperCase()}&paciente=${detalle.nombreCompleo}&fechaTicket=${detalle.fechaEmision}`;
+        args["endpoint"] = `http://localhost:3002/printer-ticket/v1/turnero?turno=${detalle.turno}&sucursal=${detalle.nombreSucursalTurnero.toUpperCase()}&paciente=${detalle.nombreCompleo}&fechaTicket=${detalle.fechaEmision}&nombreMuestraTurnero=${dataParametrosGenerales.nombreMuestraTurnero}`;
         args["method"] = "GET";
         const data = await call(args);
         if(data.code == 200){
             console.log(data)
         }
-        return;*/
+        return;
         // {
         //     "turno": "TG-008",
         //     "mensajeLlegada": "WOOOW!! ERES EL NUMERO 3 PRONTO TOCA TU TURNO",
