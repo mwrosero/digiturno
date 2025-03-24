4f7d49b494f386b8f43b848be28c5c6650986cfd
Michael Rosero <michael.rosero@akold.com>
2025-03-24 17:32:57 -0500

    Ajustes

--- resources/views/digiturno/portal2.blade.php
+++ resources/views/digiturno/portal2.blade.php
@@ -2613,8 +2613,10 @@
     async function printFactura(){
         // http://localhost:3001/printer-ticket/v1/printFile?url=https://api-phantomx.veris.com.ec/reportes/v1/facturacion/comprobante_paciente?format=text_plain%26codigoEmpresa=1%26numeroTransaccion=21479281%26codigoSucursalImpresion=1%26usuarioRealizaImpresion=true
 
+        const apiUrl = `${api_url_digitales}/reportes/v1/facturacion/comprobante_paciente?format=text_plain&codigoEmpresa=1&numeroTransaccion=${datosPago.comprobantes.transacciones[0].numeroTransaccion}&codigoSucursalImpresion=${dataParametrosGenerales.caja.codigoSucursal}&usuarioRealizaImpresion=true`;
+        const encodedUrl = encodeURIComponent(apiUrl);
         let args = [];
-        args["endpoint"] = `http://localhost:3001/printer-ticket/v1/printFile?url=${api_url_digitales}/reportes/v1/facturacion/comprobante_paciente?format=text_plain&codigoEmpresa=1&numeroTransaccion=${datosPago.comprobantes.transacciones[0].numeroTransaccion}&codigoSucursalImpresion=${dataParametrosGenerales.caja.codigoSucursal}&usuarioRealizaImpresion=true`;
+        args["endpoint"] = `http://localhost:3001/printer-ticket/v1/printFile?url=${encodedUrl}`;
         args["method"] = "GET";
         args["token"] = accessToken;
         const data = await call(args);
