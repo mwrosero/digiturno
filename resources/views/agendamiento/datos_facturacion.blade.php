b1a59a0061d6b6cb493fa7424f27f366f95718ee
Michael Rosero <michael.rosero@akold.com>
2025-03-26 10:56:51 -0500

    ajustes

--- resources/views/agendamiento/datos_facturacion.blade.php
+++ resources/views/agendamiento/datos_facturacion.blade.php
@@ -490,9 +490,10 @@ Mi Veris - Citas - Datos de facturación
         }
 
         // let dataAttr = $('.item-coincidencia-selected').attr("data-rel");
-        let paciente = dataTurno.paciente;
+        let paciente = dataCita.paciente;
+        let nombreCompleto = `${dataCita.paciente.primerNombre} ${dataCita.paciente.primerApellido} ${dataCita.paciente.segundoApellido}`;
         let args = [];
-        args["endpoint"] =  `${api_url}/${api_war}/transaccion/generar_ticket?macAddress={{ $mac }}&tipoIdentificacion=${paciente.nombreTipoIdentificacion}&numeroIdentificacion=${paciente.numeroIdentificacion}&nombreCompleto=${ paciente.nombreCompleto }${url_adicional}`;
+        args["endpoint"] =  `${api_url}/${api_war}/transaccion/generar_ticket?macAddress={{ $mac }}&tipoIdentificacion=${paciente.tipoIdentificacion}&numeroIdentificacion=${paciente.numeroIdentificacion}&nombreCompleto=${ nombreCompleto }${url_adicional}`;
         //dataCita.paciente.numeroPaciente
         args["method"] = "POST";
         args["token"] = accessToken;
