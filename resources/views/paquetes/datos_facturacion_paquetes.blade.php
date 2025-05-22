02e0845da855b2dd5a27b517f273d4c1fb64731d
mwrosero <mwrosero@gmail.com>
2025-05-22 12:24:56 -0500

    Ajuste pagar en caja
    Promocion asignada

--- resources/views/paquetes/datos_facturacion_paquetes.blade.php
+++ resources/views/paquetes/datos_facturacion_paquetes.blade.php
@@ -451,6 +451,25 @@ Mi Veris - Citas - Datos de facturación
         });
     }
 
+    function fillBoxEfectivoData(){
+
+        let detalle = {
+            // "tipoServicio": "RESERVA",
+            "beneficio": {
+                "convenio": null,
+                /*temporalmente*/
+                "tarjeta": null,
+                "paquete": null
+            },
+            "codigoReserva": null,
+            "numeroOrden": null,
+            "lineaDetalleOrden": null
+        };
+
+        return detalle;
+
+    }
+
     async function validarDatosFactura(){
         let msg = "";
 
