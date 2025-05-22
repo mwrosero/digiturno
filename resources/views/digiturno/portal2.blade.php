38f0f18cba1122bc1f3f0e33bffb6b4e4808bdb0
mwrosero <mwrosero@gmail.com>
2025-05-21 23:46:08 -0500

    ajuste

--- resources/views/digiturno/portal2.blade.php
+++ resources/views/digiturno/portal2.blade.php
@@ -3679,7 +3679,7 @@
                     labelEstadoItem = `Por pagar`;
                     classEstadoItem = `text-pendiente`;
                     // if(esKiosko){
-                        if(detalle.permitePago){
+                        if(detalle.permitePago && !isMobile()){
                             if(esKiosko){
                                 elemFooterCard += `<button type="button" data-rel='${detalleRel}' class="btn flex-fill bg-veris text-white btn-pagar p-2 py-3 mt-3">
                                         Pagar
